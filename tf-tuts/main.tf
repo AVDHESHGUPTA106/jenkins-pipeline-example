60d24a4ead2d5a28b44ba448e10b52c6b6a2ca54
AVDHESHGUPTA106 <avdheshgupta.dei@gmail.com>
2023-12-16 23:17:20 +0530

    commit tf code

---
+++ tf-tuts/main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "my_vm" {
+ ami           = var.ami //Ubuntu AMI
+ instance_type = var.instance_type
+
+ tags = {
+   Name = var.name_tag,
+ }
+}
+
+data "aws_region" "current" {}
+
